a8995e37c9358ef2b6542b31206901463b87685f
dumazert <rebeccadumazert@hotmail.fr>
2020-03-24 15:44:54 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <TestComponent url="http://google.com" text="hey hey vueJS" boolean="true" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";s
+import TestComponent from "./components/TestComponent";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    TestComponent
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{text}}</h1>
+    <a :href="url">Click here !</a>
+    <p>{{boolean}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  props: {
+    url: String,
+    text: String,
+    boolean: Boolean
+  }
+};
+</script>
+
+<style scoped></style>
